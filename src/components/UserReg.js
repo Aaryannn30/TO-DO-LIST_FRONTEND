e5c20ff8f315ae84f40f969a357a1c17d89f52e1
Aaryannn30 <aryanmaru1230@gmail.com>
2024-11-30 13:32:46 +0530

    FINAL

--- src/components/UserReg.js
+++ src/components/UserReg.js
@@ -56,53 +56,53 @@ function UserReg() {
                 const errors = error.response.data.errors;
 
                 //to get alerts for schema validation errors
+                const errorMessage = ''
                 for (const key in errors) {
                     errorMessage += `${key.charAt(0).toUpperCase() + key.slice(1)}: ${errors[key]}\n`;
                 }
                 Toast.fire({
-                    icon: "success",
-                    title: 'User Registered Successfully.',
+                    icon: "error",
+                    title: errorMessage,
                 });
                 navigate('/login')
             } else {
                 Toast.fire({
-                    icon: "success",
-                    title: 'An error occurred. Please try again.'
-                });
-            }
+                    icon: "error",
+                    title: 'An error occurred. Please try again.'})
+            }; // Fallback error message
         }
-    };
+    }
 
-    return (
-        <>
-            <Navbar />
-            <div className="flex justify-center items-center mt-56">
-                <div className="max-w-md relative flex flex-col p-10 rounded-md text-black bg-slate-200 ">
-                    <div className="text-2xl font-bold mb-2 text-[#1e0e4b] text-center">Welcome to <span className="text-[#7747ff]">To-Do List</span></div>
-                    <div className="text-sm font-normal mb-4 text-center text-[#1e0e4b]">Register your account</div>
-                    <form className="flex flex-col gap-3" onSubmit={registerUser}>
-                        <div className="block relative">
-                            <label htmlFor="name" className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2">Name</label>
-                            <input type="text" id="name" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0" />
-                        </div>
-                        <div className="block relative">
-                            <label htmlFor="email" className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2">Email</label>
-                            <input type="text" id="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0" />
-                        </div>
-                        <div className="block relative">
-                            <label htmlFor="password" className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2">Password</label>
-                            <input type="password" id="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0" />
-                        </div>
-                        <div>
-                            <a className="text-sm text-[#7747ff]" href="#">
-                            </a></div>
-                        <button type="submit" className="bg-[#7747ff] w-max m-auto px-6 py-2 rounded text-white text-sm font-normal">Submit</button>
-                    </form>
-                    <div className="text-sm text-center mt-[1.6rem]">already have an account? <Link className="text-sm text-[#7747ff]" to="/login">Log in!</Link></div>
-                </div>
+return (
+    <>
+        <Navbar />
+        <div className="flex justify-center items-center mt-56">
+            <div className="max-w-md relative flex flex-col p-10 rounded-md text-black bg-slate-200 ">
+                <div className="text-2xl font-bold mb-2 text-[#1e0e4b] text-center">Welcome to <span className="text-[#7747ff]">To-Do List</span></div>
+                <div className="text-sm font-normal mb-4 text-center text-[#1e0e4b]">Register your account</div>
+                <form className="flex flex-col gap-3" onSubmit={registerUser}>
+                    <div className="block relative">
+                        <label htmlFor="name" className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2">Name</label>
+                        <input type="text" id="name" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0" />
+                    </div>
+                    <div className="block relative">
+                        <label htmlFor="email" className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2">Email</label>
+                        <input type="text" id="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0" />
+                    </div>
+                    <div className="block relative">
+                        <label htmlFor="password" className="block text-gray-600 cursor-text text-sm leading-[140%] font-normal mb-2">Password</label>
+                        <input type="password" id="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0" />
+                    </div>
+                    <div>
+                        <a className="text-sm text-[#7747ff]" href="#">
+                        </a></div>
+                    <button type="submit" className="bg-[#7747ff] w-max m-auto px-6 py-2 rounded text-white text-sm font-normal">Submit</button>
+                </form>
+                <div className="text-sm text-center mt-[1.6rem]">already have an account? <Link className="text-sm text-[#7747ff]" to="/login">Log in!</Link></div>
             </div>
-        </>
-    )
+        </div>
+    </>
+)
 }
 
 export default UserReg;
